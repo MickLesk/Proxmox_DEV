9aea1effe50d0d5606ed32da0c54694d1c0eb427
CanbiZ <mickey.leskowitz@gmail.com>
2024-04-09 10:44:14 +0200

    Add Sniffnet

---
+++ ct/sniffnet.sh
@@ -0,0 +1,107 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/MickLesk/Proxmox_DEV/main/misc/build.func)
+# Copyright (c) 2021-2024 tteck
+# Author: tteck
+# Co-Author: MickLesk (Canbiz)
+# License: MIT
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
+# Source: https://github.com/matze/wastebin
+
+
+function header_info {
+clear
+cat <<"EOF"
+   _____       _ ________           __ 
+  / ___/____  (_) __/ __/___  ___  / /_
+  \__ \/ __ \/ / /_/ /_/ __ \/ _ \/ __/
+ ___/ / / / / / __/ __/ / / /  __/ /_  
+/____/_/ /_/_/_/ /_/ /_/ /_/\___/\__/  
+                                                                
+EOF
+}
+header_info
+echo -e "Loading..."
+APP="Sniffnet"
+var_disk="10"
+var_cpu="4"
+var_ram="2048"
+var_os="debian"
+var_version="12"
+variables
+color
+catch_errors
+
+function default_settings() {
+  CT_TYPE="1"
+  PW=""
+  CT_ID=$NEXTID
+  HN=$NSAPP
+  DISK_SIZE="$var_disk"
+  CORE_COUNT="$var_cpu"
+  RAM_SIZE="$var_ram"
+  BRG="vmbr0"
+  NET="dhcp"
+  GATE=""
+  APT_CACHER=""
+  APT_CACHER_IP=""
+  DISABLEIP6="no"
+  MTU=""
+  SD=""
+  NS=""
+  MAC=""
+  VLAN=""
+  SSH="no"
+  VERB="no"
+  echo_default
+}
+function update_script() {
+header_info
+if [[ ! -d /opt/sniffnet ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
+if (( $(df /boot | awk 'NR==2{gsub("%","",$5); print $5}') > 80 )); then
+  read -r -p "Warning: Storage is dangerously low, continue anyway? <y/N> " prompt
+  [[ ${prompt,,} =~ ^(y|yes)$ ]] || exit
+fi
+msg_info "Stopping Sniffnet"
+systemctl stop Sniffnet
+msg_ok "Stopped Sniffnet"
+
+msg_info "Updating Sniffnet"
+RELEASE=$(curl -s https://api.github.com/repos/GyulyVGC/sniffnet/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }') &>/dev/null
+cd /opt
+if [ -d sniffnet_bak ]; then
+  rm -rf sniffnet_bak
+fi
+mv sniffnet sniffnet_bak
+wget -q --no-check-certificate "https://codeload.github.com/GyulyVGC/sniffnet/zip/refs/tags/${RELEASE}"
+unzip -q ${RELEASE} &>/dev/null
+CLEAN_RELEASE=$(echo "$RELEASE" | sed 's/^v//')
+mv "sniffnet-${CLEAN_RELEASE}" /opt/sniffnet
+cd /opt/sniffnet
+cargo update -q 
+cargo build -q --release
+msg_ok "Updated Sniffnet"
+
+msg_info "Starting Sniffnet"
+systemctl start Sniffnet
+msg_ok "Started Sniffnet"
+
+msg_info "Cleaning Up"
+cd /opt
+rm -R ${RELEASE} 
+rm -R sniffnet_bak 
+msg_ok "Cleaned"
+msg_ok "Updated Successfully"
+exit
+}
+
+start
+build_container
+description
+
+msg_info "Setting Container to Normal Resources"
+pct set $CTID -cores 2
+msg_ok "Set Container to Normal Resources"
+
+msg_ok "Completed Successfully!\n"
+echo -e "${APP} Setup should be reachable by going to the following URL.
+         ${BL}http://${IP}${CL} \n"
---
+++ install/sniffnet-install.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2024 tteck
+# Author: tteck
+# Co-Author: MickLesk (Canbiz)
+# License: MIT
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
+# Source: https://github.com/GyulyVGC/sniffnet
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies (Patience)"
+$STD apt-get install -y --no-install-recommends \
+  libpcap-dev \
+  libasound2-dev \
+  libfontconfig1-dev \
+  libgtk-3-dev \
+  build-essential \
+  unzip \
+  pkg-config \
+  curl \
+  sudo \
+  git \
+  make \
+  mc
+msg_ok "Installed Dependencies"
+
+msg_info "Installing Rust (Patience)" 
+$STD bash <(curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs) -y
+source ~/.cargo/env
+msg_ok "Installed Rust" 
+
+msg_info "Installing Sniffnet (Patience)" 
+cd /opt
+RELEASE=$(curl -s https://api.github.com/repos/GyulyVGC/sniffnet/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
+$STD wget -q --no-check-certificate "https://codeload.github.com/GyulyVGC/sniffnet/zip/refs/tags/${RELEASE}"
+$STD unzip -q ${RELEASE}
+CLEAN_RELEASE=$(echo "$RELEASE" | sed 's/^v//')
+mv "sniffnet-${CLEAN_RELEASE}" sniffnet
+rm -R ${RELEASE} 
+cd sniffnet
+cargo build -q --release
+msg_ok "Installed Sniffnet"
+
+msg_info "Creating Service"
+cat <<EOF >/etc/systemd/system/sniffnet.service
+[Unit]
+Description=Start Sniffnet Service
+After=network.target
+
+[Service]
+User=root
+WorkingDirectory=/opt/sniffnet
+ExecStart=/root/.cargo/bin/cargo run -q --release
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable -q --now sniffnet.service
+msg_ok "Created Service"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+$STD apt-get autoremove
+$STD apt-get autoclean
+msg_ok "Cleaned"
