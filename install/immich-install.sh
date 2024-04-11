2eed0d24513a72fcead42835ccc2c51d0d124a6e
CanbiZ <mickey.leskowitz@gmail.com>
2024-04-11 11:21:32 +0200

    Update immich-install.sh

--- install/immich-install.sh
+++ install/immich-install.sh
@@ -54,7 +54,7 @@ msg_info "Setting up Database"
 sudo sh -c 'echo "deb https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
-$STD apt-get install -y \
+sudo apt-get install -y \
     postgresql-16 \
     postgresql-contrib-16 \
     postgresql-server-dev-all \
