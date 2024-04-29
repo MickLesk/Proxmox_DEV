35aa303f7419f1aac82d3d9d197d22d7c0f51834
CanbiZ <mickey.leskowitz@gmail.com>
2024-04-29 13:27:15 +0200

    fix update & remove unnassacarry

--- ct/ersatztv.sh
+++ ct/ersatztv.sh
@@ -69,14 +69,9 @@ msg_info "Updating ErsatzTV"
 RELEASE=$(curl -s https://api.github.com/repos/ErsatzTV/ErsatzTV/releases | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
 LATEST_RELEASE=$(echo $RELEASE | awk '{print $1}')
 cd /opt
-if [ -d ErsatzTV_bak ]; then
-  rm -rf ErsatzTV_bak
-fi
-mv ErsatzTV ErsatzTV_bak
 wget -q --no-check-certificate "https://github.com/ErsatzTV/ErsatzTV/releases/download/${LATEST_RELEASE}/ErsatzTV-${LATEST_RELEASE}-linux-x64.tar.gz"
 tar -xf ErsatzTV-${LATEST_RELEASE}-linux-x64.tar.gz 
 mv ErsatzTV-${LATEST_RELEASE}-linux-x64 ErsatzTV
-rm -R ErsatzTV-${LATEST_RELEASE}-linux-x64.tar.gz 
 msg_ok "Updated ErsatzTV"
 
 msg_info "Starting ErsatzTV"
@@ -85,8 +80,7 @@ msg_ok "Started ErsatzTV"
 
 msg_info "Cleaning Up"
 cd /opt
-rm -R ErsatzTV-${LATEST_RELEASE}-linux-x64.tar.gz
-rm -R ErsatzTV_bak 
+rm -R ErsatzTV-${LATEST_RELEASE}-linux-x64.tar.gz 
 msg_ok "Cleaned"
 msg_ok "Updated Successfully"
 exit
