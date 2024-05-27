4653ea42e603ec046c3a1188f1fb39ac111fe9af
CanbiZ <mickey.leskowitz@gmail.com>
2024-05-27 13:19:05 +0200

    Fix some things

--- install/koel-install.sh
+++ install/koel-install.sh
@@ -55,26 +55,23 @@ msg_info "Setting up PHP & NodeJS"
 sudo curl -sSLo /usr/share/keyrings/deb.sury.org-php.gpg https://packages.sury.org/php/apt.gpg
 sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
 $STD sudo apt update
-$STD sudo apt install php8.3 php8.3-{bcmath,bz2,cli,common,curl,fpm,gd,intl,mbstring,mysql,sqlite3,xml,zip}
-$STD sudo a2enconf php8.3-fpm
+$STD sudo apt install -y php8.3 php8.3-{bcmath,bz2,cli,common,curl,fpm,gd,intl,mbstring,mysql,sqlite3,xml,zip}
+sudo a2enconf php8.3-fpm
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 $STD apt-get install nodejs -y
 $STD sudo npm install --global yarn 
 msg_ok "PHP & NodeJS successfully setup" 
 
 msg_info "Installing Koel(Patience)"
-#wget https://getcomposer.org/download/latest-stable/composer.phar
-#mv composer.phar /usr/local/bin/composer
-#sudo chmod +x /usr/local/bin/composer
 wget -O composer-setup.php https://getcomposer.org/installer
-php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer --quiet
 cd /opt
 KOEL_VERSION=$(wget -q https://github.com/koel/koel/releases/latest -O - | grep "title>Release" | cut -d " " -f 4)
 wget https://github.com/koel/koel/releases/download/${KOEL_VERSION}/koel-${KOEL_VERSION}.zip
 unzip -q koel-${KOEL_VERSION}.zip
 cd koel
-composer update
-composer install
+composer update -q
+composer install -q
 sudo sed -i "s/DB_CONNECTION=.*/DB_CONNECTION=pgsql/" /opt/koel/.env
 sudo sed -i "s/DB_HOST=.*/DB_HOST=localhost/" /opt/koel/.env
 sudo sed -i "s/DB_DATABASE=.*/DB_DATABASE=$DB_NAME/" /opt/koel/.env
@@ -84,8 +81,6 @@ sudo sed -i "s|DB_PASSWORD=.*|DB_PASSWORD=$DB_PASS|" /opt/koel/.env
 sudo sed -i 's|MEDIA_PATH=.*|MEDIA_PATH=/opt/koel_media|' /opt/koel/.env
 
 msg_ok "Installed Koel"
-
-pgsql
 
 #msg_info "Set up web services"
 #cat <<EOF >/etc/nginx/conf.d/ampache.conf
