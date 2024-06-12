bec87ef0486984ad6c039db87990eb1cb39b12bc
CanbiZ <mickey.leskowitz@gmail.com>
2024-06-12 13:06:56 +0200

    MeTube LXC

---
+++ ct/metube.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/MickLesk/Proxmox_DEV/main/misc/build.func)
+# Copyright (c) 2021-2024 tteck
+# Author: MickLesk (Canbiz)
+# License: MIT
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
+
+function header_info {
+clear
+cat <<"EOF"
+    __  ___   ______      __       
+   /  |/  /__/_  __/_  __/ /_  ___ 
+  / /|_/ / _ \/ / / / / / __ \/ _ \
+ / /  / /  __/ / / /_/ / /_/ /  __/
+/_/  /_/\___/_/  \__,_/_.___/\___/ 
+                                                                
+EOF
+}
+header_info
+echo -e "Loading..."
+APP="MeTube"
+var_disk="6"
+var_cpu="1"
+var_ram="1024"
+var_os="debian"
+var_version="12"
+variables
+color
+catch_errors
+
+function default_settings() {
+  CT_TYPE="1"
+  PW=""
+  CT_ID=$NEXTID
+  HN=$NSAPP
+  DISK_SIZE="$var_disk"
+  CORE_COUNT="$var_cpu"
+  RAM_SIZE="$var_ram"
+  BRG="vmbr0"
+  NET="dhcp"
+  GATE=""
+  APT_CACHER=""
+  APT_CACHER_IP=""
+  DISABLEIP6="no"
+  MTU=""
+  SD=""
+  NS=""
+  MAC=""
+  VLAN=""
+  SSH="no"
+  VERB="no"
+  echo_default
+}
+
+function update_script() {
+header_info
+if [[ ! -d /opt/metube ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
+
+	msg_info "Stopping ${APP} Service"
+	systemctl stop metube
+	msg_ok "Stopped ${APP} Service"
+
+	msg_info "Updating ${APP} to latest Git"
+	if [ -d "/opt/metube" ]; then
+    mv /opt/metube /opt/metube_bak
+	fi
+	git clone https://github.com/alexta69/metube /opt/metube  >/dev/null 2>&1
+	cd cd /opt/metube/ui
+	npm install >/dev/null 2>&1
+	node_modules/.bin/ng build >/dev/null 2>&1
+	cd /opt/metube
+	pip3 install pipenv >/dev/null 2>&1
+	pipenv install >/dev/null 2>&1
+	pipenv run python3 app/main.py >/dev/null 2>&1
+
+	if [ -d "/opt/metube_bak" ]; then
+    rm -rf /opt/metube_bak
+	fi
+  msg_ok "Updated ${APP} to latest Git"
+
+  msg_info "Starting ${APP} Service"
+  systemctl start metube
+  sleep 1
+  msg_ok "Started ${APP} Service"
+  msg_ok "Updated Successfully!\n"
+else
+  msg_ok "No update required. ${APP} is already at ${RELEASE}"
+fi
+exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${APP} Setup should be reachable by going to the following URL.
+         ${BL}http://${IP}:8081${CL} \n"
