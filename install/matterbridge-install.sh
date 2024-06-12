377148bc3631a047b607637ca54eb4342f816e56
CanbiZ <mickey.leskowitz@gmail.com>
2024-06-12 10:41:07 +0200

    Update matterbridge-install.sh

--- install/matterbridge-install.sh
+++ install/matterbridge-install.sh
@@ -26,10 +26,16 @@ $STD apt-get install -y --no-install-recommends \
   mc
 msg_ok "Installed Dependencies"
 
-msg_info "Install NodeJS / NPM"
-$STD curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-$STD apt install -y nodejs 
-msg_ok "Installed NodeJS / NPM"
+msg_info "Setting up Node.js Repository"
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
+msg_ok "Set up Node.js Repository"
+
+msg_info "Installing Node.js"
+$STD apt-get update
+$STD apt-get install -y nodejs
+msg_ok "Installed Node.js"
 
 msg_info "Install Matterbridge" 
 RELEASE=$(curl -s https://api.github.com/repos/Luligu/matterbridge/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
@@ -38,8 +44,7 @@ unzip -q ${RELEASE}.zip
 mv matterbridge-${RELEASE} /opt/matterbridge
 rm -R ${RELEASE}.zip 
 cd /opt/matterbridge
-$STD npm install
-$STD npm run build
+$STD npm ci
 msg_ok "Installed Matterbridge"
 
 msg_info "Creating Service"
