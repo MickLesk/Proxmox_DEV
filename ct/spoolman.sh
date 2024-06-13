871b13d59f5b7ef1c67a7dcec2380bda94a149b5
CanbiZ <mickey.leskowitz@gmail.com>
2024-06-13 10:15:13 +0200

    Update spoolman.sh

--- ct/spoolman.sh
+++ ct/spoolman.sh
@@ -74,8 +74,8 @@ wget -q https://github.com/Donkie/Spoolman/releases/download/${RELEASE}/spoolman
 unzip -q spoolman.zip -d spoolman 
 echo "${RELEASE}" >/opt/${APPLICATION}_version.txt
 cd spoolman
-python3 -m venv .venv >/dev/null 2>&1
-source .venv/bin/activate >/dev/null 2>&1
+python3 -m venv .venv 
+source .venv/bin/activate 
 pip3 install -r requirements.txt >/dev/null 2>&1
 cp .env.example .env
 chmod +x scripts/*.sh
