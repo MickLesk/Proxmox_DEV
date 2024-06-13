82219024e52ecfe59029584715703f9932824e79
CanbiZ <mickey.leskowitz@gmail.com>
2024-06-13 13:23:54 +0200

    Update spoolman.sh

--- ct/spoolman.sh
+++ ct/spoolman.sh
@@ -98,3 +98,5 @@ build_container
 description
 
 msg_ok "Completed Successfully!\n"
+echo -e "${APP} Setup should be reachable by going to the following URL.
+         ${BL}http://${IP}:7912${CL} \n"
