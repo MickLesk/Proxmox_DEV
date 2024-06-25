86d73a180c642230eb0a20e8a03cae7fd5c4c54f
CanbiZ <mickey.leskowitz@gmail.com>
2024-06-25 13:53:43 +0200

    Update Scrunity

--- ct/scrutiny.sh
+++ ct/scrutiny.sh
@@ -52,7 +52,8 @@ function default_settings() {
   echo_default
 }
 
-function update_scrutiny() {
+
+function update_script() {
   if [[ ! -d /opt/scrutiny ]]; then
     msg_error "No ${APP} Installation Found!"
     exit
@@ -60,85 +61,80 @@ function update_scrutiny() {
 
   RELEASE=$(curl -s https://api.github.com/repos/AnalogJ/scrutiny/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
 
-  if [[ ! -f "/opt/scrutiny/scrutiny_version.txt" ]] || [[ "$RELEASE" != "$(cat /opt/scrutiny/scrutiny_version.txt)" ]]; then
-    msg_info "Stopping all Scrutiny Services"
-    systemctl stop scrutiny.service scrutiny_collector.service
-    msg_ok "Stopped all Scrutiny Services"
-
-    msg_info "Updating to ${RELEASE}"
-    cd /opt
-    rm -rf scrutiny_bak
-    mv scrutiny scrutiny_bak
-    mkdir -p /opt/scrutiny/web /opt/scrutiny/bin
-    wget -q -O /opt/scrutiny/bin/scrutiny-web-linux-amd64 "https://github.com/AnalogJ/scrutiny/releases/download/${RELEASE}/scrutiny-web-linux-amd64"
-    wget -q -O /opt/scrutiny/bin/scrutiny-collector-metrics-linux-amd64 "https://github.com/AnalogJ/scrutiny/releases/download/${RELEASE}/scrutiny-collector-metrics-linux-amd64"
-    wget -q -O /opt/scrutiny/web/scrutiny-web-frontend.tar.gz "https://github.com/AnalogJ/scrutiny/releases/download/${RELEASE}/scrutiny-web-frontend.tar.gz"
-    cd /opt/scrutiny/web && tar xvzf scrutiny-web-frontend.tar.gz --strip-components 1 -C .
-    chmod +x /opt/scrutiny/bin/scrutiny-web-linux-amd64
-    chmod +x /opt/scrutiny/bin/scrutiny-collector-metrics-linux-amd64
-    echo "${RELEASE}" > /opt/scrutiny/scrutiny_version.txt
-    msg_ok "Updated Scrutiny to $RELEASE"
-
-    msg_info "Cleaning up"
-    rm -f /opt/scrutiny/web/scrutiny-web-frontend.tar.gz
-    msg_ok "Cleaned"
-
-    msg_info "Starting all Scrutiny Services"
-    systemctl start scrutiny.service scrutiny_collector.service
-    msg_ok "Started all Scrutiny Services"
-    msg_ok "Updated Successfully!\n"
-  else
-    msg_ok "No update required. ${APP} is already at ${RELEASE}"
-  fi
-  exit
-}
-
-function start_webapp() {
-  msg_info "Stopping Scrutiny Collector Service"
-  systemctl stop scrutiny_collector.service
-  msg_ok "Stopped Scrutiny Collector Service"
-
-  msg_info "Starting Scrutiny Webapp Service"
-  systemctl start scrutiny.service
-  msg_ok "Started Scrutiny Webapp Service"
-  exit
-}
-
-function start_collector() {
-  msg_info "Stopping Scrutiny Webapp Service"
-  systemctl stop scrutiny.service
-  msg_ok "Stopped Scrutiny Webapp Service"
-
-  msg_info "Starting Scrutiny Collector Service"
-  systemctl start scrutiny_collector.service
-  msg_ok "Started Scrutiny Collector Service"
-  exit
-}
-
-function menu() {
-  OPTION=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "Scrutiny Management" --radiolist --cancel-button Exit-Script "Spacebar = Select" 15 60 3 \
-    "1" "Update Scrutiny" ON \
+  UPD=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "Scrutiny Management" --radiolist --cancel-button Exit-Script "Spacebar = Select" 11 58 3 \
+    "1" "Update Scrutiny to $RELEASE" ON \
     "2" "Start Scrutiny Webapp" OFF \
     "3" "Start Scrutiny Collector" OFF \
     3>&1 1>&2 2>&3)
-
   header_info
 
-  case $OPTION in
-    1)
-      update_scrutiny
-      ;;
-    2)
-      start_webapp
-      ;;
-    3)
-      start_collector
-      ;;
-    *)
-      msg_error "Invalid option selected. Exiting."
-      exit 1
-      ;;
-  esac
+  if [ "$UPD" == "1" ]; then
+    if [[ "${RELEASE}" != "$(cat /opt/scrutiny/scrutiny_version.txt)" ]] || [[ ! -f /opt/scrutiny/scrutiny_version.txt ]]; then
+
+      msg_info "Stopping all Scrutiny Services"
+	  WEBAPP_ACTIVE=$(systemctl is-active scrutiny.service)
+      COLLECTOR_ACTIVE=$(systemctl is-active scrutiny_collector.service)
+      systemctl stop scrutiny.service scrutiny_collector.service
+      msg_ok "Stopped all Scrutiny Services"
+
+      msg_info "Updating to ${RELEASE}"
+      cd /opt
+      rm -rf scrutiny_bak
+      mv scrutiny scrutiny_bak
+      mkdir -p /opt/scrutiny/web /opt/scrutiny/bin
+      wget -q -O /opt/scrutiny/bin/scrutiny-web-linux-amd64 "https://github.com/AnalogJ/scrutiny/releases/download/${RELEASE}/scrutiny-web-linux-amd64"
+      wget -q -O /opt/scrutiny/bin/scrutiny-collector-metrics-linux-amd64 "https://github.com/AnalogJ/scrutiny/releases/download/${RELEASE}/scrutiny-collector-metrics-linux-amd64"
+      wget -q -O /opt/scrutiny/web/scrutiny-web-frontend.tar.gz "https://github.com/AnalogJ/scrutiny/releases/download/${RELEASE}/scrutiny-web-frontend.tar.gz"
+      cd /opt/scrutiny/web && tar xvzf scrutiny-web-frontend.tar.gz --strip-components 1 -C .
+      chmod +x /opt/scrutiny/bin/scrutiny-web-linux-amd64
+      chmod +x /opt/scrutiny/bin/scrutiny-collector-metrics-linux-amd64
+      echo "${RELEASE}" > /opt/scrutiny/scrutiny_version.txt
+      msg_ok "Updated Scrutiny to $RELEASE"
+
+      msg_info "Cleaning up"
+      rm -f /opt/scrutiny/web/scrutiny-web-frontend.tar.gz
+      msg_ok "Cleaned"
+
+      if [ "$WEBAPP_ACTIVE" == "active" ]; then
+        msg_info "Starting Scrutiny Webapp Service"
+        systemctl start scrutiny.service
+        msg_ok "Started Scrutiny Webapp Service"
+      fi
+
+      if [ "$COLLECTOR_ACTIVE" == "active" ]; then
+        msg_info "Starting Scrutiny Collector Service"
+        systemctl start scrutiny_collector.service
+        msg_ok "Started Scrutiny Collector Service"
+      fi
+
+      msg_ok "Updated Successfully!\n"
+    else
+      msg_ok "No update required. ${APP} is already at ${RELEASE}"
+    fi
+    exit
+  fi
+
+  if [ "$UPD" == "2" ]; then
+    msg_info "Stopping Scrutiny Collector Service"
+    systemctl stop scrutiny_collector.service
+    msg_ok "Stopped Scrutiny Collector Service"
+
+    msg_info "Starting Scrutiny Webapp Service"
+    systemctl start scrutiny.service
+    msg_ok "Started Scrutiny Webapp Service"
+    exit
+  fi
+
+  if [ "$UPD" == "3" ]; then
+    msg_info "Stopping Scrutiny Webapp Service"
+    systemctl stop scrutiny.service
+    msg_ok "Stopped Scrutiny Webapp Service"
+
+    msg_info "Starting Scrutiny Collector Service"
+    systemctl start scrutiny_collector.service
+    msg_ok "Started Scrutiny Collector Service"
+    exit
+  fi
 }
 
 start
