d4a0221fa82d808fa8821fdc9880ac428cbe7218
CanbiZ <mickey.leskowitz@gmail.com>
2024-07-10 08:51:26 +0200

    Update urbackup-install.sh

--- install/urbackup-install.sh
+++ install/urbackup-install.sh
@@ -17,25 +17,21 @@ update_os
 
 msg_info "Installing Dependencies"
 $STD apt-get install -y curl
-$STD apt-get install -y sqlite3
-$STD apt-get install -y libfuse2
 $STD apt-get install -y sudo
 $STD apt-get install -y mc
 msg_ok "Installed Dependencies"
 
 msg_info "Installing UrBackup"
-RELEASE=$(wget -q -O - https://hndl.urbackup.org/Server/latest/debian/bookworm/ | grep -oP 'urbackup-server_\K[\d\.]+(?=_amd64\.deb)' | head -1)
-wget -q https://hndl.urbackup.org/Server/latest/debian/bookworm/urbackup-server_${RELEASE}_amd64.deb 
-$STD sudo DEBIAN_FRONTEND=noninteractive dpkg -i urbackup-server_${RELEASE}_amd64.deb
-$STD sudo apt install -f
-echo "${RELEASE}" >/opt/${APPLICATION}_version.txt
+echo 'deb http://download.opensuse.org/repositories/home:/uroni/Debian_12/ /' | >/etc/apt/sources.list.d/home:uroni.list
+curl -fsSL https://download.opensuse.org/repositories/home:uroni/Debian_12/Release.key | gpg --dearmor | >/etc/apt/trusted.gpg.d/home_uroni.gpg
+$STD sudo apt update
+$STD sudo apt install urbackup-server
 msg_ok "Installed UrBackup"
 
 motd_ssh
 customize
 
 msg_info "Cleaning up"
-rm -f urbackup-server_${RELEASE}_amd64.deb
 $STD apt-get -y autoremove
 $STD apt-get -y autoclean
 msg_ok "Cleaned"
