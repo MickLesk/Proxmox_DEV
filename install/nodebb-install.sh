d226a4585113c2dde11c40b8d2ecfa0a1dbad55c
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-09-04 15:19:17 +0200

    Update nodebb-install.sh

--- install/nodebb-install.sh
+++ install/nodebb-install.sh
@@ -112,7 +112,7 @@ cat <<EOF >/opt/nodebb/config.json
     "port": "4567"
 }
 EOF
-$STD ./nodebb setup
+#$STD ./nodebb setup
 echo "${CLEAN_RELEASE}" >/opt/${APPLICATION}_version.txt
 msg_ok "Installed NodeBB"
 
