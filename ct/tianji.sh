71581e10f8c5dc5000a1f86a091201ad49979438
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-09-10 10:25:44 +0200

    Improve Resources

--- ct/tianji.sh
+++ ct/tianji.sh
@@ -20,8 +20,8 @@ header_info
 echo -e "Loading..."
 APP="Tianji"
 var_disk="7"
-var_cpu="1"
-var_ram="1024"
+var_cpu="2"
+var_ram="2048"
 var_os="debian"
 var_version="12"
 variables
@@ -86,6 +86,11 @@ exit
 start
 build_container
 description
+
+msg_info "Setting Container to Normal Resources"
+pct set $CTID -memory 1024
+pct set $CTID -cores 1
+msg_ok "Set Container to Normal Resources"
 
 msg_ok "Completed Successfully!\n"
 echo -e "${APP} Setup should be reachable by going to the following URL.
