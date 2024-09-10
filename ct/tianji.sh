120c876ee3b7e9d8a7f5d8812737cdd052b75ac3
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-09-10 11:20:41 +0200

    Update tianji.sh

--- ct/tianji.sh
+++ ct/tianji.sh
@@ -19,9 +19,9 @@ EOF
 header_info
 echo -e "Loading..."
 APP="Tianji"
-var_disk="7"
-var_cpu="2"
-var_ram="2048"
+var_disk="10"
+var_cpu="4"
+var_ram="4096"
 var_os="debian"
 var_version="12"
 variables
