c0159b091f2d0a800415cab799a793da3ee8f27c
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-10-16 09:14:00 +0200

    Pixelfed LXC

--- install/pixelfed-install.sh
+++ install/pixelfed-install.sh
@@ -15,27 +15,47 @@ network_check
 update_os
 
 msg_info "Installing Dependencies (Patience)"
-$STD apt-get install -y --no-install-recommends \
+$STD apt-get install -y \
   build-essential \
   gpg \
   curl \
   sudo \
   git \
-  php8.2-{bz2,pgsql,curl,sqlite3,zip,xml} \
+  gnupg2 \
+  ca-certificates \
+  lsb-release \
+  php8.2-full \
+  composer \
+  redis-server \
+  ffmpeg \
+  jpegoptim \
+  optipng \
+  pngquant \
   make \
   mc
 msg_ok "Installed Dependencies"
 
-msg_info "Installing Pixelfed (Patience)"
-RELEASE=$(curl -s https://api.github.com/repos/pixelfed/pixelfed/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
-wget -q "https://github.com/pixelfed/pixelfed/archive/refs/tags/${RELEASE}.zip"
-unzip -q ${RELEASE}.zip 
-mv pixelfed-${RELEASE:1} /opt/pixelfed
-rm -R ${RELEASE}.zip 
-cd /opt/pixelfed
-composer install --no-ansi --no-interaction --optimize-autoloader
-cp .env.example .env
+msg_info "Configure Redis Socket"
+sed -i 's/^port .*/port 0/' /etc/redis/redis.conf
+sed -i 's|^# unixsocket .*|unixsocket /run/redis/redis.sock|' /etc/redis/redis.conf
+sed -i 's/^# unixsocketperm .*/unixsocketperm 770/' /etc/redis/redis.conf
+systemctl restart redis
+msg_ok "Redis Socket configured"
 
+msg_info "Add pixelfed user"
+useradd -rU -s /bin/bash pixelfed
+msg_ok "Pixelfed User Added"
+
+msg_info "Configure PHP-FPM for Pixelfed"
+cp /etc/php/8.2/fpm/pool.d/www.conf /etc/php/8.2/fpm/pool.d/pixelfed.conf
+sed -i 's/\[www\]/\[pixelfed\]/' /etc/php/8.2/fpm/pool.d/pixelfed.conf
+sed -i 's/^user = www-data/user = pixelfed/' /etc/php/8.2/fpm/pool.d/pixelfed.conf
+sed -i 's/^group = www-data/group = pixelfed/' /etc/php/8.2/fpm/pool.d/pixelfed.conf
+sed -i 's|^listen = .*|listen = /run/php-fpm/pixelfed.sock|' /etc/php/8.2/fpm/pool.d/pixelfed.conf
+systemctl restart php8.2-fpm
+msg_ok "successfully configured PHP-FPM"
+
+msg_info "Setup Postgres Database"
 DB_NAME=pixelfed_db
 DB_USER=pixelfed_user
 DB_ENCODING=utf8
@@ -50,26 +70,35 @@ sed -i -e "s|DB_CONNECTION=.*|DB_CONNECTION=pgsql|g" \
 
 curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc|gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
 echo "deb https://apt.postgresql.org/pub/repos/apt bookworm-pgdg main" >/etc/apt/sources.list.d/pgdg.list
-#$STD apt-get update
-#$STD apt-get install -y postgresql-16
-#$STD 
-sudo -u postgres psql -c "CREATE ROLE $DB_USER WITH LOGIN PASSWORD '$DB_PASS';"
-#$STD 
-sudo -u postgres psql -c "CREATE DATABASE $DB_NAME WITH OWNER $DB_USER TEMPLATE template0;"
-sudo -u postgres psql -c "ALTER DATABASE $DB_NAME OWNER TO $DB_USER;"
-#$STD 
-sudo -u postgres psql -c "ALTER ROLE $DB_USER SET client_encoding TO 'utf8';"
-sudo -u postgres psql -c "GRANT CREATE ON SCHEMA public TO $DB_USER;"
-#$STD 
-sudo -u postgres psql -c "ALTER ROLE $DB_USER SET default_transaction_isolation TO 'read committed';"
-#$STD 
-sudo -u postgres psql -c "ALTER ROLE $DB_USER SET timezone TO 'UTC'"
+$STD apt-get update
+$STD apt-get install -y postgresql-17
+$STD sudo -u postgres psql -c "CREATE ROLE $DB_USER WITH LOGIN PASSWORD '$DB_PASS';"
+$STD sudo -u postgres psql -c "CREATE DATABASE $DB_NAME WITH OWNER $DB_USER TEMPLATE template0;"
+$STD sudo -u postgres psql -c "ALTER DATABASE $DB_NAME OWNER TO $DB_USER;"
+$STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET client_encoding TO 'utf8';"
+$STD sudo -u postgres psql -c "GRANT CREATE ON SCHEMA public TO $DB_USER;" 
+$STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET default_transaction_isolation TO 'read committed';"
+$STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET timezone TO 'UTC'"
 echo "" >>~/pixelfed.creds
-echo -e "Pixelfed Database Name: \e[32m$DB_NAME\e[0m" >>~/pixelfed.creds
-echo -e "Pixelfed Database User: \e[32m$DB_USER\e[0m" >>~/pixelfed.creds
-echo -e "Pixelfed Database Password: \e[32m$DB_PASS\e[0m" >>~/pixelfed.creds
+echo -e "Pixelfed Database Name: $DB_NAME" >>~/pixelfed.creds
+echo -e "Pixelfed Database User: $DB_USER" >>~/pixelfed.creds
+echo -e "Pixelfed Database Password: $DB_PASS" >>~/pixelfed.creds
 export $(cat /opt/pixelfed/.env |grep "^[^#]" | xargs)
+msg_ok "Set up PostgreSQL Database successfully"
 
+msg_info "Installing Pixelfed (Patience)"
+RELEASE=$(curl -s https://api.github.com/repos/pixelfed/pixelfed/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
+wget -q "https://github.com/pixelfed/pixelfed/archive/refs/tags/${RELEASE}.zip"
+unzip -q ${RELEASE}.zip 
+mv pixelfed-${RELEASE:1} /opt/pixelfed
+rm -R ${RELEASE}.zip 
+cd /opt/pixelfed
+cd /opt/pixelfed
+sudo chown -R www-data:www-data . 
+sudo find . -type d -exec chmod 755 {} \;  
+sudo find . -type f -exec chmod 644 {} \;  
+composer install --no-ansi --no-interaction --optimize-autoloader
+cp .env.example .env
 php artisan key:generate
 php artisan storage:link
 php artisan migrate --force
@@ -78,12 +107,7 @@ php artisan instance:actor
 php artisan passport:keys
 php artisan route:cache
 php artisan view:cache
-
-
-/usr/bin/python3 /opt/tandoor/manage.py migrate >/dev/null 2>&1
-/usr/bin/python3 /opt/tandoor/manage.py collectstatic --no-input >/dev/null 2>&1
-/usr/bin/python3 /opt/tandoor/manage.py collectstatic_js_reverse >/dev/null 2>&1
-msg_ok "Set up PostgreSQL Database"
+msg_ok "Pixelfed successfully set up"
 
 msg_info "Creating Services"
 cat <<EOF >/etc/systemd/system/pixelfed.service
@@ -93,41 +117,19 @@ After=network.target
 Requires=postgresql
 Requires=php-fpm
 Requires=redis
-Requires=nginx
 
 [Service]
 Type=simple
-ExecStart=/usr/bin/php /usr/share/webapps/pixelfed/artisan horizon
-User=http
+ExecStart=/usr/bin/php /opt/pixelfed/artisan serve --host=0.0.0.0 --port=8000
+WorkingDirectory=/opt/pixelfed
+User=root
 Restart=on-failure
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
-cat << 'EOF' >/etc/nginx/conf.d/tandoor.conf
-server {
-    listen 8002;
-    #access_log /var/log/nginx/access.log;
-    #error_log /var/log/nginx/error.log;
-    client_max_body_size 128M;
-    # serve media files
-    location /static/ {
-        alias /opt/tandoor/staticfiles/;
-    }
-
-    location /media/ {
-        alias /opt/tandoor/mediafiles/;
-    }
-
-    location / {
-        proxy_set_header Host $http_host;
-        proxy_pass http://unix:/opt/tandoor/tandoor.sock;
-    }
-}
-EOF
-systemctl reload nginx
-systemctl enable -q --now gunicorn_tandoor
+systemctl enable -q --now pixelfed.service
 msg_ok "Created Services"
 
 motd_ssh
