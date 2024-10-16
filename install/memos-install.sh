a5f1924f24ac0be1c7b7b34deb84325620ceb57b
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-10-16 15:51:51 +0200

    corepack

--- install/memos-install.sh
+++ install/memos-install.sh
@@ -54,6 +54,7 @@ unzip -q ${RELEASE}.zip
 mv memos-${RELEASE:1} /opt/memos
 rm -R ${RELEASE}.zip 
 cd /opt/memos/web
+corepack enable
 pnpm install --frozen-lockfile
 pnpm build
 cp -r /opt/memos/web/dist /opt/memos/server/router/frontend
