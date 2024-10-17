adb1a01aacdd83ba960431ff3942832dad5c6fad
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-10-17 09:39:18 +0200

    Update memos-install.sh

--- install/memos-install.sh
+++ install/memos-install.sh
@@ -47,16 +47,19 @@ set -o pipefail
 msg_ok "Installed Golang"
 
 msg_info "Installing Memos (Patience)"
-RELEASE=$(curl -s https://api.github.com/repos/usememos/memos/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
-export BUF_TOKEN=7e6b142af8b96e87f9ad11e36559197c78b4a2c4ad9750cb4d14b5ab4344ae76
-cd /opt
-wget -q "https://github.com/usememos/memos/archive/refs/tags/${RELEASE}.zip"
-unzip -q ${RELEASE}.zip 
-mv memos-${RELEASE:1} /opt/memos
-rm -R ${RELEASE}.zip 
+#RELEASE=$(curl -s https://api.github.com/repos/usememos/memos/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
+#export BUF_TOKEN=7e6b142af8b96e87f9ad11e36559197c78b4a2c4ad9750cb4d14b5ab4344ae76
+#cd /opt
+#wget -q "https://github.com/usememos/memos/archive/refs/tags/${RELEASE}.zip"
+#unzip -q ${RELEASE}.zip 
+#mv memos-${RELEASE:1} /opt/memos2
+#rm -R ${RELEASE}.zip 
+#cd /opt/memos2/web
+sudo git clone https://github.com/usememos/memos.git /opt/memos
 cd /opt/memos/web
-pnpm i --frozen-lockfile
-npm run build
+sudo npm install --global pnpm
+sudo pnpm install --frozen-lockfile
+sudo pnpm build
 cp -r /opt/memos/web/dist /opt/memos/server/router/frontend/dist
 go build -o memos ./bin/memos/main.go
 mkdir -p /opt/memos_data
