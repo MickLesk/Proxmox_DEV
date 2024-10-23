1b99a9f8229042ffda211271f621d0f7f923f005
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-10-23 11:41:40 +0200

    Adding Crontabs

--- install/wallos-install.sh
+++ install/wallos-install.sh
@@ -58,6 +58,19 @@ $STD systemctl reload apache2
 curl -q http://localhost/endpoints/db/migrate.php
 msg_ok "Installed Wallos"
 
+msg_info "Setting up Crontabs" 
+cat <<EOF > /opt/wallos.cron
+0 1 * * * php /opt/wallos/endpoints/cronjobs/updatenextpayment.php >> /var/log/cron/updatenextpayment.log 2>&1
+0 2 * * * php /opt/wallos/endpoints/cronjobs/updateexchange.php >> /var/log/cron/updateexchange.log 2>&1
+0 8 * * * php /opt/wallos/endpoints/cronjobs/sendcancellationnotifications.php >> /var/log/cron/sendcancellationnotifications.log 2>&1
+0 9 * * * php /opt/wallos/endpoints/cronjobs/sendnotifications.php >> /var/log/cron/sendnotifications.log 2>&1
+*/2 * * * * php /opt/wallos/endpoints/cronjobs/sendverificationemails.php >> /var/log/cron/sendverificationemail.log 2>&1
+*/2 * * * * php /opt/wallos/endpoints/cronjobs/sendresetpasswordemails.php >> /var/log/cron/sendresetpasswordemails.log 2>&1
+0 */6 * * * php /opt/wallos/endpoints/cronjobs/checkforupdates.php >> /var/log/cron/checkforupdates.log 2>&1
+EOF
+crontab /opt/wallos.cron
+msg_ok "Crontabs setup"
+
 motd_ssh
 customize
 
