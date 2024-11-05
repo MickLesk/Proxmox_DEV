940119d97309ec828c4210e67ffb4d2a90219d31
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-11-05 15:34:39 +0100

    Update bookstack-install.sh

--- install/bookstack-install.sh
+++ install/bookstack-install.sh
@@ -56,8 +56,11 @@ sudo sed -i "s/DB_DATABASE=.*/DB_DATABASE=$DB_NAME/" /opt/bookstack/.env
 sudo sed -i "s/DB_USERNAME=.*/DB_USERNAME=$DB_USER/" /opt/bookstack/.env
 sudo sed -i "s/DB_PASSWORD=.*/DB_PASSWORD=$DB_PASS/" /opt/bookstack/.env
 $STD COMPOSER_ALLOW_SUPERUSER=1 composer install --no-dev --no-plugins --no-interaction
+echo "Composer Done"
 $STD php artisan key:generate --no-interaction
+echo "key generate Done"
 $STD php artisan migrate --no-interaction
+echo "migrate Done"
 chown www-data:www-data -R /opt/bookstack /opt/bookstack/bootstrap/cache /opt/bookstack/public/uploads /opt/bookstack/storage 
 chmod -R 755 /opt/bookstack /opt/bookstack/bootstrap/cache /opt/bookstack/public/uploads /opt/bookstack/storage 
 chmod -R 775 /opt/bookstack/storage /opt/bookstack/bootstrap/cache /opt/bookstack/public/uploads
