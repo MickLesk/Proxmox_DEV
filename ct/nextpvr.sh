958e58596d443a3ff2d67d35bd4cec762b90b234
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-11-14 13:18:45 +0100

    Update nextpvr.sh

--- ct/nextpvr.sh
+++ ct/nextpvr.sh
@@ -56,6 +56,7 @@ function default_settings() {
 function update_script() {
 header_info
 if [[ ! -f /opt/nextpvr-helper.deb ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
+check_container_ressources
 if (( $(df /boot | awk 'NR==2{gsub("%","",$5); print $5}') > 80 )); then
   read -r -p "Warning: Storage is dangerously low, continue anyway? <y/N> " prompt
   [[ ${prompt,,} =~ ^(y|yes)$ ]] || exit
