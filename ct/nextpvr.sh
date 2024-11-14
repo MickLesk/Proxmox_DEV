ff5b018c6f251a6974f4ec362401300d7e797a23
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-11-14 13:35:20 +0100

    Update nextpvr.sh

--- ct/nextpvr.sh
+++ ct/nextpvr.sh
@@ -9,17 +9,17 @@ source <(curl -s https://raw.githubusercontent.com/MickLesk/Proxmox_DEV/main/mis
 function header_info {
 clear
 cat <<"EOF"
-    _   __          __  ____ _    ______ 
-   / | / /__  _  __/ /_/ __ \ |  / / __ \
-  /  |/ / _ \| |/_/ __/ /_/ / | / / /_/ /
- / /|  /  __/>  </ /_/ ____/| |/ / _, _/ 
-/_/ |_/\___/_/|_|\__/_/     |___/_/ |_|  
-                                         
+    __  ___      _____ ____    __ 
+   /  |/  /_  __/ ___// __ \  / / 
+  / /|_/ / / / /\__ \/ / / / / /  
+ / /  / / /_/ /___/ / /_/ / / /___
+/_/  /_/\__, //____/\___\_\/_____/
+       /____/                     
 EOF
 }
 header_info
 echo -e "Loading..."
-APP="NextPVR"
+APP="MySQL"
 var_disk="4"
 var_cpu="1"
 var_ram="1024"
@@ -52,7 +52,6 @@ function default_settings() {
   VERB="no"
   echo_default
 }
-
 function update_script() {
 header_info
 check_container_ressources
