028e1271c492c16b3bcb069554aae4e64ef0beb6
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-11-15 10:29:18 +0100

    Update nextpvr.sh

--- ct/nextpvr.sh
+++ ct/nextpvr.sh
@@ -54,8 +54,8 @@ function default_settings() {
 }
 function update_script() {
 header_info
-check_storage
-check_container_ressources
+check_container_storage
+check_container_resources
 if [[ ! -f /opt/nextpvr-helper.deb ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
 msg_info "Stopping ${APP}"
 systemctl stop nextpvr-server
