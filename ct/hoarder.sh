c66b6823744d9fbea7c631e05c823352a6392a90
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-11-15 12:50:16 +0100

    Update hoarder.sh

--- ct/hoarder.sh
+++ ct/hoarder.sh
@@ -19,9 +19,9 @@ EOF
 header_info
 echo -e "Loading..."
 APP="Hoarder"
-var_disk="12"
-var_cpu="2"
-var_ram="2048"
+var_disk="10"
+var_cpu="4"
+var_ram="4096"
 var_os="debian"
 var_version="12"
 variables
