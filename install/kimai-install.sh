a4641c168c7b9cd1681f81f1014f98aa2307331e
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-11-20 14:09:34 +0100

    Update kimai-install.sh

--- install/kimai-install.sh
+++ install/kimai-install.sh
@@ -59,8 +59,8 @@ unzip -q ${RELEASE}.zip
 mv kimai-${RELEASE} /opt/kimai
 cd /opt/kimai
 COMPOSER_ALLOW_SUPERUSER=1
-$STD composer require symfony/flex
 $STD composer install --no-dev --optimize-autoloader --no-plugins --no-scripts --no-interaction
+composer require symfony/flex
 cp .env.dist .env
 sed -i "/^DATABASE_URL=/c\DATABASE_URL=mysql://$DB_USER:$DB_PASS@127.0.0.1:3306/$DB_NAME?charset=utf8mb4&serverVersion=$MYSQL_VERSION" /opt/kimai/.env
 $STD bin/console kimai:install -n
