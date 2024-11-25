1c1b25d5fde8b1a83a82da80e196a144b2fb5403
vhsdream <67816022+vhsdream@users.noreply.github.com>
2024-11-24 19:45:15 -0500

    forgot about the branch name in the URLs

--- ct/hoarder.sh
+++ ct/hoarder.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/vhsdream/prox-script-dev/main/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/vhsdream/prox-script-dev/hoarder-update/misc/build.func)
 # Copyright (c) 2021-2024 tteck
 # Author: MickLesk (Canbiz)
 # License: MIT
