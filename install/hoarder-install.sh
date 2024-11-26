32564e17332cf2aca143457e0547aae0816bee43
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-11-26 08:37:43 +0100

    Zabbix DEV

--- install/hoarder-install.sh
+++ install/hoarder-install.sh
@@ -111,7 +111,6 @@ motd_ssh
 customize
 
 msg_info "Cleaning up"
-rm -R /tmp/hoarder
 $STD apt-get -y autoremove
 $STD apt-get -y autoclean
 msg_ok "Cleaned"
---
+++ install/zabbix-install.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2024 tteck
+# Author: tteck (tteckster)
+# License: MIT
+# https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y \
+  curl \
+  sudo \
+  mc
+msg_ok "Installed Dependencies"
+
+msg_info "Installing Zabbix"
+RELEASE=$(curl -s https://repo.zabbix.com/zabbix/7.0/debian/pool/main/z/zabbix-release/ | grep -oP 'zabbix-release_[0-9.-]+(?=\+debian12_all\.deb)' | sort -V | tail -n 1)
+wget -q "https://repo.zabbix.com/zabbix/7.0/debian/pool/main/z/zabbix-release/${RELEASE}+debian12_all.deb" -O /tmp/zabbix-release.deb
+AGENT2_RELEASE=$(curl -s https://repo.zabbix.com/zabbix/7.0/debian/pool/main/z/zabbix-agent2/ | grep -oP 'zabbix-agent2_[0-9.-]+(?=\+debian12_amd64\.deb)' | sort -V | tail -n 1)
+wget -q "https://repo.zabbix.com/zabbix/7.0/debian/pool/main/z/zabbix-agent2/${AGENT2_RELEASE}+debian12_amd64.deb" -O /tmp/zabbix-agent2.deb
+$STD dpkg -i /tmp/zabbix-release.deb
+$STD dpkg -i /tmp/zabbix-agent2.deb
+$STD apt-get update
+$STD apt-get install -y zabbix-server-pgsql zabbix-frontend-php php8.2-pgsql zabbix-apache-conf zabbix-sql-scripts zabbix-agent2
+echo "${RELEASE}" >"/opt/${APPLICATION}_version.txt"
+msg_ok "Installed Zabbix"
+
+msg_info "Setting up PostgreSQL"
+$STD apt-get install -y postgresql
+DB_NAME=zabbixdb
+DB_USER=zabbix
+DB_PASS=$(openssl rand -base64 18 | tr -dc 'a-zA-Z0-9' | cut -c1-13)
+$STD sudo -u postgres psql -c "CREATE ROLE $DB_USER WITH LOGIN PASSWORD '$DB_PASS';"
+$STD sudo -u postgres psql -c "CREATE DATABASE $DB_NAME WITH OWNER $DB_USER ENCODING 'UTF8' TEMPLATE template0;"
+$STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET client_encoding TO 'utf8';"
+$STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET default_transaction_isolation TO 'read committed';"
+$STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET timezone TO 'UTC'"
+zcat /usr/share/zabbix-sql-scripts/postgresql/server.sql.gz | sudo -u $DB_USER psql $DB_NAME &>/dev/null
+sed -i "s/^DBName=.*/DBName=$DB_NAME/" /etc/zabbix/zabbix_server.conf
+sed -i "s/^DBUser=.*/DBUser=$DB_USER/" /etc/zabbix/zabbix_server.conf
+sed -i "s/^# DBPassword=.*/DBPassword=$DB_PASS/" /etc/zabbix/zabbix_server.conf
+echo "" >~/zabbix.creds
+echo "zabbix Database Credentials" >>~/zabbix.creds
+echo "" >>~/zabbix.creds
+echo -e "zabbix Database User: \e[32m$DB_USER\e[0m" >>~/zabbix.creds
+echo -e "zabbix Database Password: \e[32m$DB_PASS\e[0m" >>~/zabbix.creds
+echo -e "zabbix Database Name: \e[32m$DB_NAME\e[0m" >>~/zabbix.creds
+msg_ok "Set up PostgreSQL"
+
+msg_info "Starting Services"
+systemctl restart zabbix-server zabbix-agent2 apache2
+systemctl enable -q -now zabbix-server zabbix-agent2 apache2
+msg_ok "Started Services"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+rm -rf /tmp/zabbix-release.deb
+rm -rf /tmp/zabbix-agent2.deb
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
