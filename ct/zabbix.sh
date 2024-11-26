0c3ff7fb323d1c9ed8881cac86be4c242a8b385d
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-11-26 10:11:08 +0100

    Fixcing

--- ct/zabbix.sh
+++ ct/zabbix.sh
@@ -30,7 +30,7 @@ check_container_storage
 check_container_resources
 if [[ ! -f /etc/zabbix/zabbix_server.conf ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
 msg_info "Stopping ${APP} Services"
-systemctl stop zabbix-server-pgsql zabbix-agent2
+systemctl stop zabbix-server zabbix-agent2
 msg_ok "Stopped ${APP} Services"
 
 msg_info "Updating $APP LXC"
@@ -46,7 +46,7 @@ apt-get update &>/dev/null
 apt-get install --only-upgrade zabbix-server-pgsql zabbix-frontend-php zabbix-agent2 zabbix-agent2-plugin-* &>/dev/null
 
 msg_info "Starting ${APP} Services"
-systemctl start zabbix-server-pgsql zabbix-agent2
+systemctl start zabbix-server zabbix-agent2
 systemctl restart apache2
 msg_ok "Started ${APP} Services"
 
