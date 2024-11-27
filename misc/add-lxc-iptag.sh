432cd3871b27a05449abdbdf1b2620086a73afc7
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-11-27 10:47:13 +0100

    revert something

--- misc/add-lxc-iptag.sh
+++ misc/add-lxc-iptag.sh
@@ -1,18 +1,89 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+# Copyright (c) 2024 MickLesk
+# License: MIT
+# https://github.com/MickLesk/Proxmox_DEV
+
+function header_info {
+    cat <<"EOF"
+    __   _  ________   ________      ______           
+   / /  | |/ / ____/  /  _/ __ \    /_  __/___ _____ _
+  / /   |   / /       / // /_/ /_____/ / / __ `/ __ `/
+ / /___/   / /___   _/ // ____/_____/ / / /_/ / /_/ / 
+/_____/_/|_\____/  /___/_/         /_/  \__,_/\__, /  
+                                             /____/   
+EOF
+}
+
+# Farbvariablen
+YW=$(echo "\033[33m")
+GN=$(echo "\033[1;92m")
+RD=$(echo "\033[01;31m")
+CL=$(echo "\033[m")
+BFR="\\r\\033[K"
+HOLD=" "
+CM=" ✔️ ${CL}"
+CROSS=" ✖️ ${CL}"
+
+set -o errexit
+set -o errtrace
+set -o nounset
+set -o pipefail
 set -xe
+shopt -s expand_aliases
+alias die='EXIT=$? LINE=$LINENO error_exit'
+trap die ERR
 
-# install prerequisites
-sudo apt install -y ipcalc
+function error_exit() {
+    trap - ERR
+    local reason="Unknown failure occurred."
+    local msg="${1:-$reason}"
+    local flag="${CROSS} ERROR ${CL}$EXIT@$LINE"
+    echo -e "$flag $msg" 1>&2
+    exit $EXIT
+}
 
-# install lxc-iptag
-curl -sSL https://raw.githubusercontent.com/gitsang/lxc-iptag/main/lxc-iptag -o /usr/local/bin/lxc-iptag && chmod +x /usr/local/bin/lxc-iptag
-curl -sSL https://raw.githubusercontent.com/gitsang/lxc-iptag/main/lxc-iptag.conf -o /usr/local/etc/lxc-iptag.conf
+clear
+header_info
 
-# configure lxc-iptag systemd
-curl -sSL https://raw.githubusercontent.com/gitsang/lxc-iptag/main/lxc-iptag.service -o /lib/systemd/system/lxc-iptag.service
+APP="LXC IP-Tag"
+hostname=$(hostname)
 
-# start lxc-iptag
-sudo systemctl daemon-reload
-sudo systemctl enable lxc-iptag.service
-sudo systemctl start lxc-iptag.service
+while true; do
+    read -p "This will install ${APP} on ${hostname}. Proceed? (y/n): " yn
+    case $yn in
+    [Yy]*) break ;;
+    [Nn]*) echo "Installation cancelled."; exit ;;
+    *) echo "Please answer yes or no." ;;
+    esac
+done
+
+function msg_info() {
+    local msg="$1"
+    echo -ne " ${HOLD} ${YW}${msg}..."
+}
+
+function msg_ok() {
+    local msg="$1"
+    echo -e "${BFR} ${CM} ${GN}${msg}${CL}"
+}
+
+msg_info "Installing Prerequisites"
+apt-get update &>/dev/null
+apt-get install -y ipcalc &>/dev/null
+msg_ok "Installed Prerequisites"
+
+msg_info "Setting up IP-Tag Scripts"
+curl -sSL https://raw.githubusercontent.com/MickLesk/Proxmox_DEV/refs/heads/main/misc/lxc-iptag/iptag.func -o /usr/local/bin/iptag
+chmod +x /usr/local/bin/iptag
+curl -sSL https://raw.githubusercontent.com/MickLesk/Proxmox_DEV/refs/heads/main/misc/lxc-iptag/iptag.conf -o /usr/local/etc/iptag.conf
+curl -sSL https://raw.githubusercontent.com/MickLesk/Proxmox_DEV/refs/heads/main/misc/lxc-iptag/iptag.service -o /lib/systemd/system/iptag.service
+msg_ok "Setup IP-Tag Scripts"
+
+msg_info "Configuring Systemd Service"
+systemctl daemon-reload &>/dev/null
+systemctl enable -q --now iptag.service &>/dev/null
+msg_ok "Configured and Started IPTAG Service"
+
+echo -e "\n${APP} installation completed successfully!"
+
