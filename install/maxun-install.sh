fab090e9fd89446b8a8beb6a91da0e3e9b532a2d
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-01-07 08:56:52 +0100

    fix db

--- install/maxun-install.sh
+++ install/maxun-install.sh
@@ -51,9 +51,7 @@ msg_ok "Setup Variables"
 msg_info "Install/Set up PostgreSQL Database"
 $STD sudo -u postgres psql -c "CREATE ROLE $DB_USER WITH LOGIN PASSWORD '$DB_PASS';"
 $STD sudo -u postgres psql -c "CREATE DATABASE $DB_NAME WITH OWNER $DB_USER ENCODING 'UTF8' TEMPLATE template0;"
-$STD sudo -u postgres psql -c "CREATE EXTENSION IF NOT EXISTS postgis;" $DB_NAME
 $STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET client_encoding TO 'utf8';"
-$STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET default_transaction_isolation TO 'read committed';"
 $STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET timezone TO 'UTC';"
 {
     echo "Maxun-Credentials"
