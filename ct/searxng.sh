6762167651f7ced8b74b5edffb5b40585b8a593f
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-02-07 10:49:07 +0100

    Update searxng.sh

--- ct/searxng.sh
+++ ct/searxng.sh
@@ -28,40 +28,12 @@ function update_script() {
   header_info
   check_container_storage
   check_container_resources
-  if [[ ! -d /opt/paperless-ai ]]; then
+  if [[ ! -d /usr/local/searxng/searxng-src ]]; then
     msg_error "No ${APP} Installation Found!"
     exit
   fi
-  RELEASE=$(curl -s https://api.github.com/repos/clusterzx/paperless-ai/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
-  if [[ "${RELEASE}" != "$(cat /opt/${APP}_version.txt)" ]] || [[ ! -f /opt/${APP}_version.txt ]]; then
-    msg_info "Stopping $APP"
-    systemctl stop paperless-ai
-    msg_ok "Stopped $APP"
-
-    msg_info "Updating $APP to v${RELEASE}"
-    cd /opt
-    mv /opt/paperless-ai /opt/paperless-ai_bak
-    wget -q "https://github.com/clusterzx/paperless-ai/archive/refs/tags/v${RELEASE}.zip"
-    unzip -q v${RELEASE}.zip
-    mv paperless-ai-${RELEASE} /opt/paperless-ai
-    mkdir -p /opt/paperless-ai/data
-    cp -a /opt/paperless-ai_bak/data/. /opt/paperless-ai/data/
-    cd /opt/paperless-ai
-    npm install &>/dev/null
-    echo "${RELEASE}" >/opt/${APP}_version.txt
-    msg_ok "Updated $APP to v${RELEASE}"
-
-    msg_info "Starting $APP"
-    systemctl start paperless-ai
-    msg_ok "Started $APP"
-
-    msg_info "Cleaning Up"
-    rm -rf /opt/v${RELEASE}.zip
-    rm -rf /opt/paperless-ai_bak
-    msg_ok "Cleanup Completed"
-    msg_ok "Update Successful"
-  else
-    msg_ok "No update required. ${APP} is already at v${RELEASE}"
+    if cd /usr/local/searxng/searxng-src && git pull | grep -q 'Already up to date'; then
+    msg_ok "There is currently no update available."
   fi
   exit
 }
