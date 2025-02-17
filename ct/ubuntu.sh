c219b78fdff3d23a12deebeb27099c34fa5d9160
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-02-17 11:29:04 +0100

    Update ubuntu.sh

--- ct/ubuntu.sh
+++ ct/ubuntu.sh
@@ -29,8 +29,8 @@ function update_script() {
         exit
     fi
     msg_info "Updating ${APP} LXC"
-    $STD apt-get update
-    $STD apt-get -y upgrade
+    apt-get update
+    apt-get -y upgrade
     msg_ok "Updated ${APP} LXC"
     exit
 }
